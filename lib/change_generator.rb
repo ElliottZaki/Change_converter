f7179d162e235533a2a40034af9009358e2786c2
Elliott Zaki <elliottzaki@Elliotts-Air.broadband>
2022-06-24 16:37:07 +0100

    Working programme. Tests past. Issues with floating point error. Made numbers into whole numbers as opposed to floating points

--- lib/change_generator.rb
+++ lib/change_generator.rb
@@ -1,8 +1,5 @@
 class Change_generator
 
-  attr_reader :number
-
-
 # currency_demoninations = ["£20", "£10", "£5", "£2", "£1", "50p", "20p", "10p", "5p", "2p", "1p"]
 
 CURRENCY_VALUE = Hash.new
@@ -20,10 +17,6 @@ CURRENCY_VALUE = Hash.new
 
 number = 50
 
-  # def convert(number)
-  #   convert_to_float(number)
-  # end
-
   def convert_to_float(number)
     float_number = number.to_f
     puts float_number
@@ -31,6 +24,7 @@ number = 50
 
   def convert(number)
     conversion_list = []
+    # number = number.to_f
       divisable_by_20?(number, conversion_list)
     print conversion_list
     return conversion_list
@@ -39,23 +33,79 @@ number = 50
 private
 
   def divisable_by_20?(number, conversion_list)
-    remainder = number / 20 
-    number = number % 20
+    remainder = number / 2000
+    number = number % 2000
     conversion_list.fill("£20", conversion_list.size, remainder) 
     divisable_by_10?(number, conversion_list)
   end
 
   def divisable_by_10?(number, conversion_list)
-    remainder = number / 10
-    number = number % 10
+    remainder = number / 1000
+    number = number % 1000
     conversion_list.fill("£10", conversion_list.size, remainder) 
     divisable_by_5?(number, conversion_list)
   end
 
   def divisable_by_5?(number, conversion_list)
+    remainder = number / 500
+    number = number % 500
+    conversion_list.fill("£5", conversion_list.size, remainder)
+    divisable_by_2?(number, conversion_list) 
+  end
+
+  def divisable_by_2?(number, conversion_list)
+    remainder = number / 200
+    number = number % 200
+    conversion_list.fill("£2", conversion_list.size, remainder)
+    divisable_by_1?(number, conversion_list) 
+  end
+
+  def divisable_by_1?(number, conversion_list)
+    remainder = number / 100
+    number = number % 100
+    conversion_list.fill("£1", conversion_list.size, remainder) 
+    divisable_by_0_5?(number, conversion_list)
+  end
+
+  def divisable_by_0_5?(number, conversion_list)
+    remainder = number / 50
+    number = number % 50
+    conversion_list.fill("50p", conversion_list.size, remainder)
+    divisable_by_0_2?(number, conversion_list) 
+  end
+
+  def divisable_by_0_2?(number, conversion_list)
+    remainder = number / 20
+    number = number % 20
+    conversion_list.fill("20p", conversion_list.size, remainder)
+    divisable_by_0_1?(number, conversion_list)  
+  end
+
+  def divisable_by_0_1?(number, conversion_list)
+    remainder = number / 10
+    number = number % 10
+    conversion_list.fill("10p", conversion_list.size, remainder)
+    divisable_by_0_05?(number, conversion_list)  
+  end
+
+  def divisable_by_0_05?(number, conversion_list)
     remainder = number / 5
     number = number % 5
-    conversion_list.fill("£5", conversion_list.size, remainder) 
+    conversion_list.fill("5p", conversion_list.size, remainder)
+    divisable_by_0_02?(number, conversion_list)  
+  end
+
+  def divisable_by_0_02?(number, conversion_list)
+    remainder = number / 2
+    number = number % 2
+    conversion_list.fill("2p", conversion_list.size, remainder)
+    divisable_by_0_01?(number, conversion_list)  
+  end
+
+  def divisable_by_0_01?(number, conversion_list)
+    remainder = number / 1
+    number = number % 1
+    conversion_list.fill("1p", conversion_list.size, remainder) 
   end
 end
 
--- spec/change_generator_spec.rb
+++ spec/change_generator_spec.rb
@@ -2,37 +2,87 @@ require './lib/change_generator.rb'
 
 describe Change_generator do
   let (:change_generator) {Change_generator.new}
-  # let (:currency_value) {{"£20"=>20, "£10"=>10, "£5"=>5, "£2"=>2, "£1"=>1, "50p"=>0.5, "20p"=>0.2, "10p"=>0.1, "5p"=>0.05, "2p"=>0.02, "1p"=>0.01}}
 
   it 'should respond to #convert method with 1 argument' do
     expect(change_generator).to respond_to(:convert).with(1).argument
   end
 
   describe '#convert method' do
-
+    it 'should breakdown a number into the most efficent monetary denominiations' do
+      expect(change_generator.convert(2398)).to eq (["£20", "£2", "£1", "50p", "20p", "20p", "5p", "2p", "1p"])
+    end
   end
 
   describe '#divisable_by_20? method' do
     it 'should breakdown 20 and return a £20 note' do
-      expect(change_generator.convert(20)).to eq (["£20"])
+      expect(change_generator.convert(2000)).to eq (["£20"])
     end
 
     it 'should breakdown 40 and return 2x £20 notes' do
-      expect(change_generator.convert(40)).to eq (["£20", "£20"])
+      expect(change_generator.convert(4000)).to eq (["£20", "£20"])
     end
   end
 
   describe '#divisable_by_10? method' do
     it 'should breakdown 50 and return 2x £20 notes, & 1x £10 note' do
-      expect(change_generator.convert(50)).to eq (["£20", "£20", "£10"])
-      expect(change_generator.convert(50)).not_to eq (["£10", "£10", "£10", "£10", "£10"])
+      expect(change_generator.convert(5000)).to eq (["£20", "£20", "£10"])
+      expect(change_generator.convert(5000)).not_to eq (["£10", "£10", "£10", "£10", "£10"])
     end
   end
 
   describe '#divisable_by_5? method' do
     it 'should breakdown 15 and return 1x £10 note, & 1x £5 note' do
-      expect(change_generator.convert(15)).to eq (["£10", "£5",])
+      expect(change_generator.convert(1500)).to eq (["£10", "£5",])
     end
   end
+
+  describe '#divisable_by_2? method' do
+    it 'should breakdown 7 and return 1x £5 note, & 1x £2 coin' do
+      expect(change_generator.convert(700)).to eq (["£5", "£2"])
+    end
+  end
+
+  describe '#divisable_by_1? method' do
+    it 'should breakdown 3 and return 1x £2 coin, & 1x £1 coin' do
+      expect(change_generator.convert(300)).to eq (["£2", "£1"])
+    end
+  end
+
+  describe '#divisable_by_0_50? method' do
+    it 'should breakdown 1.50 and return 1x £1 coin & 1x 50p coin' do
+      expect(change_generator.convert(150)).to eq (["£1", "50p"])
+    end
+  end
+
+  describe '#divisable_by_0_20? method' do
+    it 'should breakdown 0.75 and return 1x 50p coin & 1x 20p coin' do
+      expect(change_generator.convert(70)).to eq (["50p", "20p"])
+    end
+  end
+
+  describe '#divisable_by_0_10? method' do
+    it 'should breakdown 0.10 and return a 10p coin' do
+      expect(change_generator.convert(10)).to eq (["10p"])
+    end
+  end
+
+  describe '#divisable_by_0_05? method' do
+    it 'should breakdown 0.05 and return a 5p coin' do
+      expect(change_generator.convert(5)).to eq (["5p"])
+    end
+  end
+
+  describe '#divisable_by_0_02? method' do
+    it 'should breakdown 0.02 and return a 2p coin' do
+      expect(change_generator.convert(2)).to eq (["2p"])
+    end
+  end
+
+  describe '#divisable_by_0_01? method' do
+    it 'should breakdown 0.31 and return 1x £20p coin, a 10p coin & x 1p coin' do
+      expect(change_generator.convert(31)).to eq (["20p", "10p", "1p"])
+    end
+  end
+
 
 end
